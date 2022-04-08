bd627d52d34a80152e3d09f68d7b9d07ad899824
k-batista <kennnedy.kb@gmail.com>
2022-04-08 16:56:37 -0300

    Rename functions

--- src/inflation/adapter/load_files.clj
+++ src/inflation/adapter/load_files.clj
@@ -11,28 +11,28 @@
 
 (db/cria-schema conn)
 
-(defn execute [salary]
+(defn insert-entity [salary]
   (prn salary)
   (d/transact conn [salary]))
 
-(defn read [filename] 
+(defn read-file [filename] 
   (with-open [reader (io/reader filename)]
     (doall
      (csv/read-csv reader))))
 
-(defn teste [item, country, type]
+(defn create-price [item, country, type]
   (let [value (bigdec (nth item 2))
         month (long (Float/valueOf (nth item 1)))
         year (long (Float/valueOf (nth item 0)))]
     (model/new-price country type value year month)))
 
-(defn read-file [filename country type]
-  (doseq [item (read filename)]
+(defn load-file [filename country type]
+  (doseq [item (read-file filename)]
     (-> item 
         (first)
         (str/split #";")
-        (teste country type)
-        (execute))))
+        (create-price country type)
+        (insert-entity))))
 
-(read-file "resources/br_salary.csv" "BR" "SALARY")
-(read-file "resources/br_basic_food_basket.csv" "BR" "BFB")
+(load-file "resources/br_salary.csv" "BR" "SALARY")
+(load-file "resources/br_basic_food_basket.csv" "BR" "BFB")
